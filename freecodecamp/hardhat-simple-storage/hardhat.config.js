60f21824412990b11ca3de8c43d45bbc7d62b22b
webthethird <ratzbodell@gmail.com>
2022-06-18 18:07:49 -0500

    Add dotenv package to project

--- freecodecamp/hardhat-simple-storage/hardhat.config.js
+++ freecodecamp/hardhat-simple-storage/hardhat.config.js
@@ -1,14 +1,16 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle")
+require("dotenv/config")
+require("@nomiclabs/hardhat-etherscan")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -16,6 +18,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+    },
+  },
   solidity: "0.8.8",
-};
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+}
