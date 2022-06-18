dc2c97c3df3afc573b8d8b6cf64c0adb747b19c5
webthethird <ratzbodell@gmail.com>
2022-06-17 22:26:41 -0500

    Make prettier

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # web3-tutorials
 
 ---
-A place for me to work through Web3 tutorials in public. 
 
+A place for me to work through Web3 tutorials in public.
 
 ### Credit where credit is due
 
 ---
-I'm starting with the [free, Full-stack Web3 course](https://github.com/smartcontractkit/full-blockchain-solidity-course-js) 
+
+I'm starting with the [free, Full-stack Web3 course](https://github.com/smartcontractkit/full-blockchain-solidity-course-js)
 from [freeCodeCamp.org](freecodecamp.org) by the wonderful [Patrick Collins](https://github.com/PatrickAlphaC).
 I know I have been putting this off for far too long, but now I certainly don't have any excuse for continuing to do so!
-
 
 ## Notes
 
@@ -18,17 +18,17 @@ I know I have been putting this off for far too long, but now I certainly don't 
 
 In addition to taking notes as comments in the code itself, I'll take some broader notes here as I go along.
 
-- _Dear future employers:_ Let it be known that, while I'm starting from the beginning of the course, I have been able 
-to write (unsophisticated) Solidity contracts for years. Sure, some content in the first handful of lessons seems 
-trivial after two years of graduate-level education in computer science and even longer in-and-out-and-back into Web3.
-But the way I see it, Patrick put this course together as a masterclass on Web3, so of course there are going to be
-insightful nuggets of useful info nestled in there with the introductory stuff. Anyway, what better place is there to 
-start than from the top? All I know is I need to put what I know together with everything else I need to learn to finish
-what I started years ago, and I need to start _right now!_
+- _Dear future employers:_ Let it be known that, while I'm starting from the beginning of the course, I have been able
+  to write (unsophisticated) Solidity contracts for years. Sure, some content in the first handful of lessons seems
+  trivial after two years of graduate-level education in computer science and even longer in-and-out-and-back into Web3.
+  But the way I see it, Patrick put this course together as a masterclass on Web3, so of course there are going to be
+  insightful nuggets of useful info nestled in there with the introductory stuff. Anyway, what better place is there to
+  start than from the top? All I know is I need to put what I know together with everything else I need to learn to finish
+  what I started years ago, and I need to start _right now!_
 
 - Chainlink out-of-the-box features:
   - Price Feeds
   - Verifiable Random Numbers (VRF)
-  - Keepers: Listen for external triggers and look-up actions specified for specific contracts 
+  - Keepers: Listen for external triggers and look-up actions specified for specific contracts
   - Http GET API calls (does not come with the decentralized network of Chainlink nodes)
 - Requesting data from Chainlink nodes requires LINK
--- freecodecamp/contracts/FundMe.sol
+++ freecodecamp/contracts/FundMe.sol
@@ -4,60 +4,57 @@ pragma solidity ^0.8.7;
 import "./PriceConverter.sol";
 
 contract FundMe {
-    // Goals:
-    // 1. Get funds from users
-    // 2. Withdraw funds (only owner)
-    // 3. Set minimum funding value in USD (using Chainlink)
-    using PriceConverter for uint256;
+  // Goals:
+  // 1. Get funds from users
+  // 2. Withdraw funds (only owner)
+  // 3. Set minimum funding value in USD (using Chainlink)
+  using PriceConverter for uint256;
 
-    // Minimum funding value (in USD)
-    uint256 public minimumUSD = 50;
+  // Minimum funding value (in USD)
+  uint256 public minimumUSD = 50;
 
-    address payable public owner;
-    address[] public funders;
-    mapping(address => uint256) public fundedByAddress;
+  address payable public owner;
+  address[] public funders;
+  mapping(address => uint256) public fundedByAddress;
 
-    constructor() {
-        owner = payable(msg.sender);
+  constructor() {
+    owner = payable(msg.sender);
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Only the contract owner can withdraw funds!");
+    _;
+  }
+
+  function fund() public payable {
+    // require(getConversionRate(msg.value) >= minimumUSD * 1e18, "Below minimum funding value!");
+    require(
+      msg.value.getConversionRate() >= minimumUSD * 1e18,
+      "Below minimum funding value!"
+    );
+    funders.push(msg.sender);
+    fundedByAddress[msg.sender] += msg.value;
+  }
+
+  function withdraw() public onlyOwner {
+    for (uint256 i = 0; i < funders.length; i++) {
+      fundedByAddress[funders[i]] = 0;
     }
+    // reset the array
+    funders = new address[](0);
 
-    modifier onlyOwner() {
-        require(
-            msg.sender == owner,
-            "Only the contract owner can withdraw funds!"
-        );
-        _;
-    }
-
-    function fund() public payable {
-        // require(getConversionRate(msg.value) >= minimumUSD * 1e18, "Below minimum funding value!");
-        require(
-            msg.value.getConversionRate() >= minimumUSD * 1e18,
-            "Below minimum funding value!"
-        );
-        funders.push(msg.sender);
-        fundedByAddress[msg.sender] += msg.value;
-    }
-
-    function withdraw() public onlyOwner {
-        for (uint i = 0; i < funders.length; i++) {
-            fundedByAddress[funders[i]] = 0;
-        }
-        // reset the array
-        funders = new address[](0);
-
-        // withdraw the funds
-        // three ways to send ETH
-        // 1: transfer (2300 gas, throws error on failure)
-        // owner.transfer(address(this).balance);
-        // 2: send (2300 gas, returns bool)
-        // bool sendSuccess = owner.send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-        // 3: call (forward all gas or set gas, returns bool plus any return data)
-        //    this is currently the recommended way to send ETH
-        (bool callSuccess, bytes memory dataReturned) = owner.call{
-            value: address(this).balance
-        }("");
-        require(callSuccess, "Call failed");
-    }
+    // withdraw the funds
+    // three ways to send ETH
+    // 1: transfer (2300 gas, throws error on failure)
+    // owner.transfer(address(this).balance);
+    // 2: send (2300 gas, returns bool)
+    // bool sendSuccess = owner.send(address(this).balance);
+    // require(sendSuccess, "Send failed");
+    // 3: call (forward all gas or set gas, returns bool plus any return data)
+    //    this is currently the recommended way to send ETH
+    (bool callSuccess, bytes memory dataReturned) = owner.call{
+      value: address(this).balance
+    }("");
+    require(callSuccess, "Call failed");
+  }
 }
