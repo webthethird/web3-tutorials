b839f834f0ca1839dede33ae54c353159f8f218c
webthethird <ratzbodell@gmail.com>
2022-06-21 20:11:01 -0500

    Deploy to any network

--- freecodecamp/hardhat-fund-me/deploy/01-deploy-fund-me.js
+++ freecodecamp/hardhat-fund-me/deploy/01-deploy-fund-me.js
@@ -1,3 +1,7 @@
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
+
 // async function deploy(hre) {
 //   console.log("Deploying...")
 // }
@@ -7,8 +11,8 @@
 //   const {getNamedAccounts, deployments} = hre
 // }
 
-module.exports = async ({ getNamedAccounts, deployments, network }) => {
-    const { deploy, log } = deployments
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log, get } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
@@ -19,5 +23,39 @@ module.exports = async ({ getNamedAccounts, deployments, network }) => {
      * Solution: use a mock that simulates the behavior of the real object.
      *
      * But what about when we want to switch between chains?
+     * We still can't use a hardcoded address, because they may be different.
+     * Solution: pass an address into the constructor rather than hardcoding it.
      */
+
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    const args = [
+        /* Price feed address */
+        ethUsdPriceFeedAddress,
+    ]
+
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    /**
+     * Auto-verify on Etherscan
+     */
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
+    log("-------------------------------------------------------------")
 }
+
+module.exports.tags = ["all", "fundme"]
