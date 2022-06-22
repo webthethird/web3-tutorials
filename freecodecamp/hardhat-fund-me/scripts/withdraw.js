f91604e91b3505e0d7b76d3cda4a80a60d337b6b
webthethird <ratzbodell@gmail.com>
2022-06-21 22:52:15 -0500

    Add scripts

---
+++ freecodecamp/hardhat-fund-me/scripts/withdraw.js
@@ -0,0 +1,17 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+
+async function main() {
+    const { deployer } = await getNamedAccounts()
+    const fundMe = await ethers.getContract("FundMe", deployer)
+    console.log("Withdrawing from contract...")
+    const txResponse = await fundMe.withdraw()
+    await txResponse.wait(1)
+    console.log("Withdrawn!")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
