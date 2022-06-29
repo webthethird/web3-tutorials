174353c318ddce3c45faf4b62c513d92c2f8d71f
webthethird <ratzbodell@gmail.com>
2022-06-28 19:08:39 -0500

    FCC Lesson #9: Implement Chainlink VRF

--- freecodecamp/hardhat-lottery/contracts/Raffle.sol
+++ freecodecamp/hardhat-lottery/contracts/Raffle.sol
@@ -2,6 +2,9 @@
 
 pragma solidity ^0.8.8;
 
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 // Goals:
 // 1. enter the lottery (by paying some amount)
 // 2. Pick a random winner (verifiably random)
@@ -10,30 +13,93 @@ pragma solidity ^0.8.8;
 // Chainlink Oracle -> Randomness, Automated Execution (Keepers)
 
 error Raffle__NotEnoughETHToEnter();
+error Raffle__TransferFailed();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
-    uint256 private immutable __entranceFee;
-    address payable[] private _players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint256 private immutable i_entranceFee;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    address payable[] private s_players;
+    address payable private s_recentWinner;
 
-    constructor(uint256 entranceFee) {
-        __entranceFee = entranceFee;
+    /* Events */
+    event RaffleEnter(
+        address indexed entrant,
+        uint256 indexed entryValue,
+        uint256 totalEntrants
+    );
+
+    event RequestedRaffleWinner(uint256 indexed requestId);
+
+    event WinnerPicked(address indexed winner);
+
+    constructor(
+        address _vrfCoordinator,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(_vrfCoordinator) {
+        i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
-        if (msg.value < __entranceFee) {
+        if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHToEnter();
         }
-        _players.push(payable(msg.sender));
+        s_players.push(payable(msg.sender));
+
+        // Emit an event whenever we modify a state variable in storage
+        emit RaffleEnter(msg.sender, msg.value, s_players.length);
     }
 
-    // function pickRandomWinner() {}
+    function pickRandomWinner() external {
+        // 1. Request random number (here)
+        // 2. Once we get it, do something with it (in fulfillRandomWords)
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // gas lane
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
 
+    function fulfillRandomWords(
+        uint256, /* requestId */  // <-- tells compiler that this parameter will not be used 
+        uint256[] memory randomWords
+    ) internal override {
+        address payable recentWinner = s_players[
+            randomWords[0] % s_players.length
+        ];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
+
+    /* View / Pure Functions */
     function getEntranceFee() public view returns (uint256) {
-        return __entranceFee;
+        return i_entranceFee;
     }
 
     function getPlayer(uint256 playerIndex) public view returns (address) {
-        return _players[playerIndex];
+        return s_players[playerIndex];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
