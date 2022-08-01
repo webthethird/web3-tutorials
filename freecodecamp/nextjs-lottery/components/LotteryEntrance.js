22882f94ba0d8075825cce9d1d594ef931713dca
webthethird <ratzbodell@gmail.com>
2022-07-31 21:40:11 -0500

    Lottery Entrance basic UI

--- freecodecamp/nextjs-lottery/components/LotteryEntrance.js
+++ freecodecamp/nextjs-lottery/components/LotteryEntrance.js
@@ -1,15 +1,108 @@
 // Need a function to allow users to enter the lottery
-import { useWeb3Contract } from "react-moralis"
-import {abi} from "../constants/abi"
+import { useEffect, useState } from "react"
+import { useWeb3Contract, useMoralis } from "react-moralis"
+import { useNotification } from "web3uikit"
+import { abi, contractAddresses } from "../constants"
+import { ethers } from "ethers"
 
 export default function LotteryEntrance() {
-    const  {runContractFunction: enterRaffle} = useWeb3Contract{
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+    const [entranceFee, setEntranceFee] = useState("0")
+    const [numPlayers, setNumPlayers] = useState("0")
+    const [recentWinner, setRecentWinner] = useState("0")
+    const dispatch = useNotification()
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
         abi: abi,
-        // contractAddress: ,
-        // functionName: ,
-        // params: {},
-        // msgValue:
+        contractAddress: raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    async function updateUI() {
+        const entranceFeeFromCall = await getEntranceFee({
+            onError: (error) => console.error(error),
+        })
+        const numPlayersFromCall = await getNumPlayers({
+            onError: (error) => console.error(error),
+        })
+        const recentWinnerFromCall = await getRecentWinner({
+            onError: (error) => console.error(error),
+        })
+        setEntranceFee(entranceFeeFromCall)
+        setRecentWinner(recentWinnerFromCall.toString())
+        setNumPlayers(numPlayersFromCall)
     }
 
-    return <div>Lottery Entrance</div>
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            // try to read the raffle entrance fee
+            updateUI()
+        }
+    }, [isWeb3Enabled, recentWinner])
+
+    const { runContractFunction: enterRaffle } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "enterRaffle",
+        params: {},
+        msgValue: entranceFee,
+    })
+
+    const { runContractFunction: getNumPlayers } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getNumPlayers",
+        params: {},
+    })
+
+    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getRecentWinner",
+        params: {},
+    })
+
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNewNotification(tx)
+        updateUI()
+    }
+
+    const handleNewNotification = function () {
+        dispatch({
+            type: "info",
+            message: "Transaction Complete",
+            title: "Tx Notification",
+            position: "topR",
+            icon: "bell",
+        })
+    }
+
+    return (
+        <div>
+            Lottery Entrance
+            {raffleAddress ? (
+                <div>
+                    <button
+                        onClick={async function () {
+                            await enterRaffle({
+                                onSuccess: handleSuccess,
+                                onError: (error) => console.error(error),
+                            })
+                        }}
+                    >
+                        Enter Raffle
+                    </button>
+                    <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
+                    <div>Number of Players: {numPlayers.toString()}</div>
+                    <div>Most Recent Winner: {recentWinner}</div>
+                </div>
+            ) : (
+                <div>No Raffle Address Detected</div>
+            )}
+        </div>
+    )
 }
